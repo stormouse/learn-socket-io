73fc8318d1eb37102700bcbd0502ee20128e9a4c
Shawnc <stormouse@gmail.com>
2019-03-03 20:43:49 -0800

    let sub-parser ignore error before detecting match

--- games/reigns/parser.js
+++ games/reigns/parser.js
@@ -6,11 +6,11 @@ module.exports = {
     parse: function(message, user) {
         try {
             if(this.gameInstance == null) return false;
-            if(user.nickname.match(/^\w+$/) == null) {
-                user.socket.emit('system_notification', 'Change to your nickname to play the game.');
-                return true;
-            }
             if((captured = this.vote_action_re.exec(message)) != null) {
+                if(user.nickname.match(/^\w+$/) == null) {
+                    user.socket.emit('system_notification', 'Change to your nickname to play the game.');
+                    return true;
+                }
                 this.gameInstance.vote(user.nickname, captured[1]);
                 return true;
             }
