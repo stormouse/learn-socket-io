d22836ceabf9e95369bd30d6f69b0cb8abab1267
Congxin Cheng <congxin@usc.edu>
2019-03-05 00:18:59 -0800

    partially updated app.js

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,17 @@
 import express from 'express';
-import * as http from 'http';
-import socket from 'socket.io';
-import * as fs from 'fs';
+import http from 'http';
+import socketIO, {Socket} from 'socket.io';
+import fs from 'fs';
 import { UserOperations } from './operations/user';
-
-const app = express();
-const server = new http.Server(app);
-const io = socket(server);
 
 class User {
 	nickname: string;
-	socket: any;
-	constructor(socket: any) {
+	socket: socketIO.Socket;
+	constructor(socket: socketIO.Socket) {
 		this.socket = socket;
 		this.nickname = String(socket.request.connection.remoteAddress);
 	}
-	setNickname(nickname: string) {
+	public setNickname(nickname: string): void {
 		this.nickname = nickname;
 	}
 }
@@ -106,60 +102,87 @@ function dumpMessage() {
 	});
 }
 
-app.get('/', function (req: any, res: any) {
-	res.sendFile(__dirname + '/index.html');
-});
+class ChatServer {
+	public static readonly DEFAULT_PORT: number = 8080;
+	private readonly app: express.Application;
+	private readonly server: http.Server;
+	private readonly io: socketIO.Server;
+	private readonly port: number;
 
-io.on('connection', (socket: any) => {
-	let socket_id = JSON.stringify(socket.request.connection._peername);
+	constructor() {
+		// create express app
+		this.app = express();
+		this.app.get('/', (req: express.Request, res: express.Response) => {
+			res.sendFile(__dirname + '/index.html');
+		});
 
-	if (!globalContext.connectedUsers.hasOwnProperty(socket_id)) {
-		try {
-			globalContext.connectedUsers[socket_id] = new User(socket);
-			console.log('new connection from ' + socket.request.connection.remoteAddress);
-			io.emit('message', "Welcome friend from " + socket.request.connection.remoteAddress);
-			socket.emit('sync_history', messageCache);
-		} catch(e) {
-			console.log("error when establishing new connection.");
-		}
+		// configuration
+		this.port = Number(process.env.PORT) || ChatServer.DEFAULT_PORT;
+
+		// create http server
+		this.server = http.createServer(this.app);
+
+		// bind socketIO with the express app
+		this.io = socketIO(this.server);
 	}
 
-	socket.on('disconnect', function () {
-		console.log('user disconnected');
-		delete globalContext.connectedUsers[socket_id];
-		io.emit('message', 'someone left us ):<');
-	});
+	// use instance function to avoid the 'this' problem
+	public listen = () => {
+		this.server.listen(this.port, () => {
+			console.log('Running server on port ' + this.port);
+		});
 
-	socket.on('message', async function (msg: string) {
-		try {
-			let isCommand = false;
-			let parsers = globalContext.parsers;
-			let user = globalContext.connectedUsers[socket_id];
-			for (let i in parsers) {
-				let result = await parsers[i].parse(msg, user);
-				if (result === true) {
-					isCommand = true;
-					break;
+		this.io.on('connection', (socket: Socket) => {
+			let socket_id = JSON.stringify(socket.request.connection._peername);
+
+			if (!globalContext.connectedUsers.hasOwnProperty(socket_id)) {
+				try {
+					globalContext.connectedUsers[socket_id] = new User(socket);
+					console.log('new connection from ' + socket.request.connection.remoteAddress);
+					this.io.emit('message', "Welcome friend from " + socket.request.connection.remoteAddress);
+					socket.emit('sync_history', messageCache);
+				} catch(e) {
+					console.log("error when establishing new connection.");
 				}
 			}
-			if (!isCommand) {
-				let user = globalContext.connectedUsers[socket_id];
-				io.emit('message', user.nickname + ": " + msg);
-				messageCache.push(user.nickname + ": " + msg);
-				if(messageCache.length >= globalContext.messageCacheLines) {
-					messageCache.shift();
-				}
-				messageToDump.push(user.nickname + ": " + msg);
-				if(messageToDump.length > globalContext.singleFileMessageLimit) {
-					dumpMessage();
-				}
-			}
-		} catch (e) {
-			console.log(e);
-		}
-	});
-});
 
-server.listen(8080, () => {
-	console.log('listening on *:8080');
-});
+			socket.on('disconnect',  () => {
+				console.log('user disconnected');
+				delete globalContext.connectedUsers[socket_id];
+				this.io.emit('message', 'someone left us ):<');
+			});
+
+			socket.on('message', async (msg: string) => {
+				try {
+					let isCommand = false;
+					let parsers = globalContext.parsers;
+					let user = globalContext.connectedUsers[socket_id];
+					for (let i in parsers) {
+						let result = await parsers[i].parse(msg, user);
+						if (result === true) {
+							isCommand = true;
+							break;
+						}
+					}
+					if (!isCommand) {
+						let user = globalContext.connectedUsers[socket_id];
+						this.io.emit('message', user.nickname + ": " + msg);
+						messageCache.push(user.nickname + ": " + msg);
+						if(messageCache.length >= globalContext.messageCacheLines) {
+							messageCache.shift();
+						}
+						messageToDump.push(user.nickname + ": " + msg);
+						if(messageToDump.length > globalContext.singleFileMessageLimit) {
+							dumpMessage();
+						}
+					}
+				} catch (e) {
+					console.log(e);
+				}
+			});
+		});
+	}
+}
+
+const chatServer = new ChatServer();
+chatServer.listen();
